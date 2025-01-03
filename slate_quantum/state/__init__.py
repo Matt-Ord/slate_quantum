a3af72e0808e5ebbba44acce1f2ce81d012a15cb
Matthew Ord <matthew.ord1@gmail.com>
2025-01-03 09:57:06 +0000

    normalize_states

--- slate_quantum/state/__init__.py
+++ slate_quantum/state/__init__.py
@@ -13,6 +13,7 @@ from slate_quantum.state._state import (
     get_occupations,
     inner_product,
     normalization,
+    normalize_states,
 )
 
 __all__ = [
@@ -26,4 +27,5 @@ __all__ = [
     "get_occupations",
     "inner_product",
     "normalization",
+    "normalize_states",
 ]
