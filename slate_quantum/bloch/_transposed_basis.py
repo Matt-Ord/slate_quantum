d4597c7521120561560fc69e4d37ddd49ea9304c
Matt Ord <matthew.ord1@gmail.com>
2025-01-13 09:07:26 +0000

    Fix types

--- slate_quantum/bloch/_transposed_basis.py
+++ slate_quantum/bloch/_transposed_basis.py
@@ -150,15 +150,11 @@ class BlochTransposedBasis[
     @override
     def features(self) -> set[BasisFeature]:
         out = set[BasisFeature]()
-        if "SIMPLE_ADD" in self.inner.features:
+        if "LINEAR_MAP" in self.inner.features:
             out.add("ADD")
-            out.add("SIMPLE_ADD")
-        if "SIMPLE_MUL" in self.inner.features:
+            out.add("LINEAR_MAP")
             out.add("MUL")
-            out.add("SIMPLE_MUL")
-        if "SIMPLE_SUB" in self.inner.features:
             out.add("SUB")
-            out.add("SIMPLE_SUB")
         if "INDEX" in self.inner.features:
             out.add("INDEX")
         return out
@@ -169,7 +165,7 @@ class BlochTransposedBasis[
         lhs: np.ndarray[Any, np.dtype[DT1]],
         rhs: np.ndarray[Any, np.dtype[DT1]],
     ) -> np.ndarray[Any, np.dtype[DT1]]:
-        if "SIMPLE_ADD" not in self.features:
+        if "LINEAR_MAP" not in self.features:
             msg = "add_data not implemented for this basis"
             raise NotImplementedError(msg)
         return (lhs + rhs).astype(lhs.dtype)
@@ -178,7 +174,7 @@ class BlochTransposedBasis[
     def mul_data[DT1: np.number[Any]](
         self, lhs: np.ndarray[Any, np.dtype[DT1]], rhs: float
     ) -> np.ndarray[Any, np.dtype[DT1]]:
-        if "SIMPLE_MUL" not in self.features:
+        if "LINEAR_MAP" not in self.features:
             msg = "mul_data not implemented for this basis"
             raise NotImplementedError(msg)
         return (lhs * rhs).astype(lhs.dtype)
@@ -189,7 +185,7 @@ class BlochTransposedBasis[
         lhs: np.ndarray[Any, np.dtype[DT1]],
         rhs: np.ndarray[Any, np.dtype[DT1]],
     ) -> np.ndarray[Any, np.dtype[DT1]]:
-        if "SIMPLE_SUB" not in self.features:
+        if "LINEAR_MAP" not in self.features:
             msg = "sub_data not implemented for this basis"
             raise NotImplementedError(msg)
         return (lhs - rhs).astype(lhs.dtype)
