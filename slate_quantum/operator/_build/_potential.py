Fix gaussian variance

slate_quantum/operator/_build/_potential.py
@@ -121,7 +121,13 @@ def potential_from_function[M: SpacedLengthMetadata, E: AxisDirections, DT: np.g
         [tuple[np.ndarray[Any, np.dtype[np.floating]], ...]],
         np.ndarray[Any, np.dtype[DT]],
     ],
+    *,
+    wrapped: bool = False,
+    offset: tuple[float, ...] | None = None,
 ) -> Potential[M, E, DT]:
     """Get the potential operator."""
-    positions = _metadata.volume.fundamental_stacked_x_points(metadata)
+    positions = _metadata.volume.fundamental_stacked_x_points(
+        metadata, offset=offset, wrapped=wrapped
+    )
+
     return Potential(basis.from_metadata(metadata), fn(positions))

slate_quantum/state/_build.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
 import numpy as np
 from slate import Array, StackedMetadata, TupleBasis, basis
@@ -13,6 +13,10 @@ from slate.metadata import (
 )
 
 from slate_quantum.state._state import State
+from slate_quantum.state._state import normalize as normalize_state
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
 
 
 def wrap_displacements(
@@ -63,7 +67,7 @@ def get_displacements_x_stacked(
     )
 
 
-def build_coherent_state[M: SpacedLengthMetadata, E: AxisDirections](
+def coherent[M: SpacedLengthMetadata, E: AxisDirections](
     metadata: StackedMetadata[M, E],
     x_0: tuple[float, ...],
     k_0: tuple[float, ...],
@@ -84,3 +88,47 @@ def build_coherent_state[M: SpacedLengthMetadata, E: AxisDirections](
     norm = np.sqrt(np.sum(np.square(np.abs(data))))
 
     return State(basis.from_metadata(metadata), data / norm)
+
+
+def position[M: SpacedLengthMetadata, E: AxisDirections](
+    metadata: StackedMetadata[M, E],
+    idx: tuple[int, ...],
+) -> State[StackedMetadata[M, E]]:
+    """Get the position of a wavepacket."""
+    position_basis = basis.from_metadata(metadata)
+    data = np.zeros(metadata.shape, dtype=np.complex128)
+    idx = tuple(i % n for (i, n) in zip(idx, metadata.shape, strict=True))
+    data[idx] = 1.0
+    return State(position_basis, data)
+
+
+def momentum[M: SpacedLengthMetadata, E: AxisDirections](
+    metadata: StackedMetadata[M, E],
+    idx: tuple[int, ...],
+) -> State[StackedMetadata[M, E]]:
+    """Get the momentum of a wavepacket."""
+    momentum_basis = basis.fundamental_transformed_tuple_basis_from_metadata(metadata)
+    data = np.zeros(metadata.shape, dtype=np.complex128)
+    idx = tuple(i % n for (i, n) in zip(idx, metadata.shape, strict=True))
+    data[idx] = 1.0
+    return State(momentum_basis, data)
+
+
+def from_function[M: SpacedLengthMetadata, E: AxisDirections](
+    metadata: StackedMetadata[M, E],
+    fn: Callable[
+        [tuple[np.ndarray[Any, np.dtype[np.floating]], ...]],
+        np.ndarray[Any, np.dtype[np.complexfloating]],
+    ],
+    *,
+    normalize: bool = True,
+    offset: tuple[float, ...] | None = None,
+    wrapped: bool = False,
+) -> State[StackedMetadata[M, E]]:
+    """Get the potential operator."""
+    positions = _metadata.volume.fundamental_stacked_x_points(
+        metadata, offset=offset, wrapped=wrapped
+    )
+
+    result = State(basis.from_metadata(metadata), fn(positions))
+    return normalize_state(result) if normalize else result

slate_quantum/state/_state.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Any, cast, overload, override
 
 import numpy as np
-from slate import Array, FundamentalBasis, array, linalg, tuple_basis
+from slate import Array, FundamentalBasis, SimpleMetadata, array, linalg, tuple_basis
 from slate import basis as _basis
 from slate.basis import (
     Basis,
@@ -15,7 +15,7 @@ from slate.metadata import BasisMetadata, Metadata2D
 from slate_quantum.metadata import EigenvalueMetadata
 
 if TYPE_CHECKING:
-    from collections.abc import Iterator
+    from collections.abc import Iterable, Iterator
 
 
 class State[
@@ -68,6 +68,15 @@ def normalization(
     """
     product = inner_product(state, state)
     return np.abs(product)
+
+
+def normalize[M1: BasisMetadata](
+    state: State[M1],
+) -> State[M1]:
+    norm = normalization(state)
+    state_as_index = array.as_mul_basis(state)
+
+    return State(state_as_index.basis, state_as_index.raw_data / np.sqrt(norm))
 
 
 def get_occupations[B: Basis[BasisMetadata, Any]](
@@ -182,8 +191,28 @@ class StateList[
             final_basis, self.basis.__convert_vector_into__(self.raw_data, final_basis)
         )
 
+    @staticmethod
+    def from_states[
+        M1_: BasisMetadata,
+        B1: Basis[Any, np.complexfloating] = Basis[M1_, np.complexfloating],
+    ](
+        iter_: Iterable[State[M1_, B1]],
+    ) -> StateList[
+        SimpleMetadata,
+        M1_,
+        TupleBasis2D[Any, FundamentalBasis[SimpleMetadata], B1, None],
+    ]:
+        states = list(iter_)
+        assert all(x.basis == states[0].basis for x in states)
 
-def normalize_states[M0: BasisMetadata, M1: BasisMetadata](
+        list_basis = FundamentalBasis.from_size(len(states))
+        return StateList(
+            tuple_basis((list_basis, states[0].basis)),
+            np.array([x.raw_data for x in states]),
+        )
+
+
+def normalize_all[M0: BasisMetadata, M1: BasisMetadata](
     states: StateList[M0, M1],
 ) -> StateList[
     M0,