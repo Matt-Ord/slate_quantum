2c0a65dc1ccaf7fb9aa628b53c9a58dc15392633
Matt Ord <matthew.ord1@gmail.com>
2025-05-26 15:21:32 +0000

    Improved types again

--- slate_quantum/noise/build/__init__.py
+++ slate_quantum/noise/build/__init__.py
@@ -13,7 +13,6 @@ from slate_quantum.noise.build._build import (
     truncate_noise_operator_list,
 )
 from slate_quantum.noise.build._caldeira_leggett import (
-    CLNoiseOperatorList,
     caldeira_leggett_correlation_fn,
     caldeira_leggett_operators,
     periodic_caldeira_leggett_axis_operators,
@@ -22,7 +21,6 @@ from slate_quantum.noise.build._caldeira_leggett import (
 )
 
 __all__ = [
-    "CLNoiseOperatorList",
     "axis_kernel_from_function_stacked",
     "caldeira_leggett_correlation_fn",
     "caldeira_leggett_operators",
@@ -37,5 +35,5 @@ __all__ = [
     "temperature_corrected_operators",
     "truncate_diagonal_noise_kernel",
     "truncate_noise_kernel",
-    "truncate_noise_operator_list"
+    "truncate_noise_operator_list",
 ]
