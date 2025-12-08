f05c62511e20767902baf921a1b33408f6ac5abb
Matthew Ord <matthew.ord1@gmail.com>
2025-12-08 16:35:39 +0000

    Untested support for full quantum simulations

--- slate_quantum/dynamics/langevin/__init__.py
+++ slate_quantum/dynamics/langevin/__init__.py
@@ -1,9 +1,11 @@
 from slate_quantum.dynamics.langevin._harmonic import (  # noqa: D104
     solve_harmonic_langevin,
+    solve_harmonic_quantum_langevin,
     solve_harmonic_stable_quantum_langevin,
 )
 from slate_quantum.dynamics.langevin._periodic import (
     solve_periodic_langevin,
+    solve_periodic_quantum_langevin,
     solve_periodic_stable_quantum_langevin,
 )
 from slate_quantum.dynamics.langevin._util import (
@@ -17,7 +19,9 @@ __all__ = [
     "SSEConfig",
     "SSEMethod",
     "solve_harmonic_langevin",
+    "solve_harmonic_quantum_langevin",
     "solve_harmonic_stable_quantum_langevin",
     "solve_periodic_langevin",
+    "solve_periodic_quantum_langevin",
     "solve_periodic_stable_quantum_langevin",
 ]
--- slate_quantum/dynamics/langevin/_periodic.py
+++ slate_quantum/dynamics/langevin/_periodic.py
@@ -6,6 +6,7 @@ from slate_core import (
     Array,
     EvenlySpacedLengthMetadata,
     FundamentalBasis,
+    SimpleMetadata,
     TupleBasis,
     TupleMetadata,
     array,
@@ -14,6 +15,7 @@ from slate_core import (
 from slate_core.metadata.volume import AxisDirections, fundamental_stacked_dk
 from slate_core.util import timed
 
+from slate_quantum import StateList
 from slate_quantum.dynamics.langevin._util import (
     LangevinParameters,
     SSEConfig,
@@ -31,6 +33,8 @@ if TYPE_CHECKING:
     from slate_core import Basis, Ctype
     from sse_solver_py import PeriodicLangevinSystemParameters  # type: ignore lib
 
+    from slate_quantum import State
+    from slate_quantum.dynamics import RealizationListBasis
     from slate_quantum.dynamics._realization import RealizationListIndexMetadata
     from slate_quantum.operator._operator import Operator, OperatorBasis
 
@@ -232,3 +236,95 @@ def solve_periodic_stable_quantum_langevin[
         (FundamentalBasis.from_size(n_trajectories), times_basis)
     ).upcast()
     return (Array(out_basis, alpha_res), Array(out_basis, data[:, :, 1]))
+
+
+@timed
+def solve_periodic_quantum_langevin[
+    MT: TimeMetadata,
+    M: EvenlySpacedLengthMetadata,
+    E: AxisDirections,
+    MS: SimpleMetadata,
+](
+    initial_state: tuple[
+        complex, complex, State[Basis[MS], np.dtype[np.complexfloating]]
+    ],
+    times: Basis[MT, Ctype[np.complexfloating]],
+    parameters: LangevinParameters,
+    potential: Operator[
+        OperatorBasis[TupleMetadata[tuple[M, ...], E]], np.dtype[np.complexfloating]
+    ],
+    **kwargs: Unpack[SSEConfig],
+) -> tuple[
+    Array[
+        Basis[RealizationListIndexMetadata[MT]],
+        np.dtype[np.complexfloating],
+    ],
+    Array[
+        Basis[RealizationListIndexMetadata[MT]],
+        np.dtype[np.complexfloating],
+    ],
+    StateList[RealizationListBasis[MT, MS], np.dtype[np.complexfloating]],
+]:
+    r"""Solve the dynamics of a harmonic oscillator coupled to a thermal bath using the harmonic Langevin equation.
+
+    Raises
+    ------
+    ImportError
+        If the rust sse_solver_py is not installed
+    """
+    ts = datetime.datetime.now(tz=datetime.UTC)
+
+    if sse_solver_py is None:
+        msg = "sse_solver_py is not installed, please install it using `pip install slate_quantum[sse_solver_py]`"
+        raise ImportError(msg)
+
+    times_basis = basis.as_index(times)
+    normalized_params = parameters.normalized_parameters
+    normalized_times = rescale_times(
+        times_basis.metadata().values[times_basis.points],
+        in_parameter=parameters,
+        out_parameter=normalized_params,
+    )
+
+    target_delta = kwargs.get("target_delta", 1e-3)
+    n_trajectories = kwargs.get("n_trajectories", 1)
+    data = sse_solver_py.solve_periodic_quantum_langevin(  # type: ignore lib
+        (
+            rescale_alpha(
+                initial_state[0],
+                in_parameter=parameters,
+                out_parameter=normalized_params,
+            ),
+            initial_state[1],
+            initial_state[2].as_array().tolist(),
+        ),
+        get_internal_parameters(
+            normalized_params, get_dimensionless_potential_params(potential, parameters)
+        ),
+        sse_solver_py.SimulationConfig(  # type: ignore lib
+            times=normalized_times.tolist(),
+            dt=target_delta,
+            delta=(None, target_delta, None) if kwargs.get("adaptive") else None,
+            n_trajectories=n_trajectories,
+            n_realizations=1,
+            method=kwargs.get("method", "Euler"),
+        ),
+    )
+    data = np.array(cast("list[complex]", data)).reshape(  # pyright: ignore[reportUnnecessaryCast]
+        (n_trajectories, normalized_times.size, -1)
+    )
+
+    te = datetime.datetime.now(tz=datetime.UTC)
+    print(f"solve_harmonic_langevin took: {(te - ts).total_seconds()} sec")  # noqa: T201
+
+    alpha_res = rescale_alpha(
+        data[:, :, 0], out_parameter=parameters, in_parameter=normalized_params
+    )
+    out_basis = TupleBasis(
+        (FundamentalBasis.from_size(n_trajectories), times_basis)
+    ).upcast()
+    out_states = StateList(
+        TupleBasis((out_basis, basis.as_fundamental(initial_state[2].basis))).upcast(),
+        data[:, 0, 2:],
+    )
+    return (Array(out_basis, alpha_res), Array(out_basis, data[:, :, 1]), out_states)
